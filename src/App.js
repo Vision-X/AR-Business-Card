dec75e446a18daa8550d04cdd5eabf95f8c17c97
Vision-X <michael.s.marlow@gmail.com>
2018-09-07 16:03:21 -0600

    adjustment to a text element moving its position to under AR primitive box

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
                     color="black"
             >
             </a-text>*/}
-            <a-text value="Michael Marlow bruhhhhh"
-                    position="-1 1.0 0"
+            <a-text value="Michael Marlow"
+                    position="-1 -1.0 0"
                     rotation="-90 0 0"
                     color="white"
             >
