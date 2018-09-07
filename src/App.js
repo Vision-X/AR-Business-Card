0f1ad235fa137d2f91db54502fe14a798b6f6fba
Vision-X <michael.s.marlow@gmail.com>
2018-09-07 15:45:02 -0600

    adjust text rotation for nested text element, changes color for non nested text to differentiate

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ class App extends Component {
           <Scene embedded arjs>
             <a-text value="Michael Marlow bruhhhhh"
                     position="0 0.7 0"
-                    rotation="90 0 0"
-                    color="black"
+                    rotation="-90 0 0"
+                    color="green"
             >
             </a-text>
             {/*<!-- create your content here. just a box for now -->*/}
             <a-box position='0 0.5 0' material='color: blue; opacity: 0.5;'>
               <a-text value="Michael Marlow bruhhhhh"
                       position="0 1.0 0"
+                      rotation="-90 0 0"
                       color="black"
               ></a-text>
             </a-box>
