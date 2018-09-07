8f7edaea48fd76df241d1b373d0b42b521401420
Vision-X <michael.s.marlow@gmail.com>
2018-09-07 17:05:20 -0600

    add animations for testing

--- src/App.js
+++ src/App.js
@@ -21,13 +21,27 @@ class App extends Component {
             >
             </a-text>
             {/*<!-- create your content here. just a box for now -->*/}
-            <a-box position='0 0.5 0' material='color: blue; opacity: 0.5;'>
-              {/*<a-text value="Michael Marlow bruhhhhh"
-                      position="0 1.0 0"
+            <a-entity geometry="primitive: box; depth=0.0 height=0.5 width=0.5"
+                      position='0 0.5 0'
+                      material='color: blue; opacity: 0.5;'
+
+            >
+              <a-animation attribute="material.color"
+                         begin="2000"
+                         from="red"
+                         to="blue"
+              >
+              <a-animation attribute="scale"
+                           begin="2000"
+                           to="0 0.5 0.5"
+              >
+              </a-animation>
+              <a-text value="Michael Marlow bruhhhhh"
+                      position="-1 1.0 0"
                       rotation="-90 0 0"
                       color="black"
-              ></a-text>*/}
-            </a-box>
+              ></a-text>
+          </a-entity>
             {/*<!-- define a camera which will move according to the marker position -->*/}
             <a-marker-camera preset='hiro'></a-marker-camera>
           </Scene>
