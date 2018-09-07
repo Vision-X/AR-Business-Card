25afe1583f998f90f876b0cf4b534ddef23c4dfc
Vision-X <michael.s.marlow@gmail.com>
2018-09-07 14:27:31 -0600

    add basic functionality, testing development on firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import 'aframe-react';
+import { Scene } from 'aframe-react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+          <Scene embedded arjs>
+            {/*<!-- create your content here. just a box for now -->*/}
+            <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+            {/*<!-- define a camera which will move according to the marker position -->*/}
+            <a-marker-camera preset='hiro'></a-marker-camera>
+          </Scene>
     );
   }
 }
