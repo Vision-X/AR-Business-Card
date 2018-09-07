a82a7374825199c9c6abc0d107f151ef38f586b1
Vision-X <michael.s.marlow@gmail.com>
2018-09-07 14:54:23 -0600

    add text element for testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import 'aframe-react';
 import { Scene } from 'aframe-react';
 import logo from './logo.svg';
@@ -8,8 +8,13 @@ class App extends Component {
   render() {
     return (
           <Scene embedded arjs>
+            <a-text value="Michael Marlow bruhhhhh"
+                    position="0 0 0.5"
+                    color="black"
+                    >
+            </a-text>
             {/*<!-- create your content here. just a box for now -->*/}
-            <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+            <a-box position='0 0.5 0' material='color: blue, opacity: 0.5;'></a-box>
             {/*<!-- define a camera which will move according to the marker position -->*/}
             <a-marker-camera preset='hiro'></a-marker-camera>
           </Scene>
