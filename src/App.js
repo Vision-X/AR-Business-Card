fa1aed1e05df3fa38ad851250439b666f8cd10b7
Vision-X <michael.s.marlow@gmail.com>
2018-09-07 15:51:54 -0600

    adjust text on each axis, add mirror text behind on z axis

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ class App extends Component {
     return (
           <Scene embedded arjs>
             <a-text value="Michael Marlow bruhhhhh"
-                    position="0 0.7 0"
+                    position="-0.5 0.9 0"
                     rotation="-90 0 0"
-                    color="green"
+                    color="black"
+            >
+            <a-text value="Michael Marlow bruhhhhh"
+                    position="-0.5 1.0 0.1"
+                    rotation="-90 0 0"
+                    color="white"
             >
             </a-text>
             {/*<!-- create your content here. just a box for now -->*/}
             <a-box position='0 0.5 0' material='color: blue; opacity: 0.5;'>
-              <a-text value="Michael Marlow bruhhhhh"
+              {/*<a-text value="Michael Marlow bruhhhhh"
                       position="0 1.0 0"
                       rotation="-90 0 0"
                       color="black"
-              ></a-text>
+              ></a-text>*/}
             </a-box>
             {/*<!-- define a camera which will move according to the marker position -->*/}
             <a-marker-camera preset='hiro'></a-marker-camera>
